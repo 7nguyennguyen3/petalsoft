d7b1c05f5aeed9b0eb5976dbcc615cb8f90d66fd
Shield <7nguyennguyen3@gmail.com>
2024-05-19 12:16:53 -0700

    Testing stripe webhook endpoint

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -7,6 +7,7 @@ import { db } from "@/db";
 export async function POST(request: NextRequest) {
   try {
     const body = await request.text();
+    const response = JSON.parse(body);
     const signature = headers().get("stripe-signature");
 
     if (!signature) {
@@ -14,49 +15,52 @@ export async function POST(request: NextRequest) {
     }
 
     const event = stripe.webhooks.constructEvent(
-      body,
+      response,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
 
-    if (event.type === "checkout.session.completed") {
-      if (!event.data.object.customer_details?.email) {
-        throw new Error("Missing user email");
+    if (event.type === "payment_intent.succeeded") {
+      const paymentIntent = event.data.object as Stripe.PaymentIntent;
+
+      if (
+        !paymentIntent.metadata.email ||
+        !paymentIntent.metadata.shippingAddress
+      ) {
+        throw new Error("Missing user email or shipping address");
       }
 
-      const session = event.data.object as Stripe.Checkout.Session;
+      const shippingAddress = JSON.parse(
+        paymentIntent.metadata.shippingAddress
+      );
 
-      const shippingAddress = session.shipping_details!.address;
+      const customerShippingAddress = await db.shippingAddress.create({
+        data: {
+          name: paymentIntent.metadata.name,
+          street: shippingAddress.line1,
+          city: shippingAddress.city,
+          country: shippingAddress.country,
+          postalCode: shippingAddress.postal_code,
+        },
+      });
 
-      if (session.payment_status === "paid") {
-        const customerShippingAddress = await db.shippingAddress.create({
-          data: {
-            name: session.customer_details?.name!,
-            street: shippingAddress?.line1!,
-            city: shippingAddress?.city!,
-            country: shippingAddress?.country!,
-            postalCode: shippingAddress?.postal_code!,
-          },
-        });
+      await db.order.create({
+        data: {
+          userId: paymentIntent.metadata.userId,
+          total: paymentIntent.amount_received / 100,
+          isPaid: true,
+          status: "awaiting_shipment",
+          shippingAddressId: customerShippingAddress.id,
+        },
+      });
 
-        await db.order.create({
-          data: {
-            userId: session.metadata!.userId,
-            total: session.amount_total! / 100,
-            isPaid: true,
-            status: "awaiting_shipment",
-            shippingAddressId: customerShippingAddress.id,
-          },
-        });
-
-        return NextResponse.json({ result: event, ok: true });
-      }
+      return NextResponse.json({ result: event, ok: true });
     }
   } catch (error) {
     console.error(error);
 
     return NextResponse.json(
-      { message: "Something went wrong", ok: false },
+      { message: "event failed", ok: false },
       { status: 500 }
     );
   }
